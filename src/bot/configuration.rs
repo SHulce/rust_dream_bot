500a11153df6c1dd91b555e47ebfcdf9255cbb4d
Shawn Hulce <shulce@asu.edu>
2019-11-21 11:09:44 -0600

    Now utilizing proper derive Serialize/Deserialize from Serde.

--- src/bot/configuration.rs
+++ src/bot/configuration.rs
@@ -1,7 +1,12 @@
+extern crate serde;
 extern crate serde_json;
 
 use std::fs;
+use serde::{Serialize, Deserialize};
 
+const _CONFIG_FILENAME: &str = "resources/botconfig.json";
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
     pub bot_token: String,
     pub giphy_api_key: String,
@@ -10,31 +15,14 @@ pub struct Config {
 
 impl Config {
 
-    pub fn to_json_string(&self) -> String {
-        let config_content = vec!["{\n", "\"bot_token\": \"", &self.bot_token, 
-        "\",\n\"giphy_api_key\": \"", &self.giphy_api_key, "\",\n\"next_session\": \"", &self.next_session.as_str(),
-        "\"\n}"];
-        config_content.join("")
+    pub fn new() -> Config {
+        let config_content = fs::read_to_string(_CONFIG_FILENAME).expect("Failed to open config");
+        serde_json::from_str(&config_content).unwrap()
     }
 
     pub fn save_config(&self) -> std::io::Result<()> {
-        let config_filename = "botconfig.json";
-        let config_json = &self.to_json_string();
-        fs::write(config_filename, config_json)?;  
+        let config_json = serde_json::to_string(&self).unwrap();
+        fs::write(_CONFIG_FILENAME, config_json)?;  
         Ok(())
     }
-}
-
-pub fn initialize_config() -> Config {
-    let config_filename = "botconfig.json";
-    let config_content = fs::read_to_string(config_filename).expect("Failed to open config");
-    parse_config(config_content)
-}
-
-fn parse_config(contents: String) -> Config {
-    let data: serde_json::value::Value = serde_json::from_str(&contents).unwrap();
-    //eprintln!("Data in parse_config: {:?}", data);
-    Config { bot_token: String::from(data["bot_token"].as_str().unwrap()), 
-    giphy_api_key: String::from(data["giphy_api_key"].as_str().unwrap()),
-    next_session: String::from(data["next_session"].as_str().unwrap()) }
 }
